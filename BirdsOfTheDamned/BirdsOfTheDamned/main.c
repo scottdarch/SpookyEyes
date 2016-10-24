41e39919f22ab427719b4a1da047ef7a347e01b6
Scott Dixon <scottd.nerd@gmail.com>
2016-10-23 19:27:14 -0700

    Handling interrupt properly now.

--- BirdsOfTheDamned/BirdsOfTheDamned/main.c
+++ BirdsOfTheDamned/BirdsOfTheDamned/main.c
@@ -33,30 +33,39 @@ EMPTY_INTERRUPT(INT0_vect);
 
 EMPTY_INTERRUPT(WDT_vect);
 
+// Mantissa (LM[7:4]): Four most significant bits of mantissa lower threshold
+// Exponent (LE[3:0]): Exponent bits lower threshold
+// Lower lux threshold = 2(exponent) x mantissa x 0.045
+// Exponent = 8xLE3 + 4xLE2 + 2xLE1 + LE0
+// Mantissa = 128xLM7 + 64xLM6 + 32xLM5 + 16xLM4
+static const uint8_t lower_threshold_lux = 0x1;
+    
 void configure_max44009() {
-    // Mantissa (LM[7:4]): Four most significant bits of mantissa lower threshold
-    // Exponent (LE[3:0]): Exponent bits lower threshold
-    // Lower lux threshold = 2(exponent) x mantissa x 0.045
-    // Exponent = 8xLE3 + 4xLE2 + 2xLE1 + LE0
-    // Mantissa = 128xLM7 + 64xLM6 + 32xLM5 + 16xLM4
-    static const uint8_t lower_threshold_lux = (2 << 4) | (1 << 0);
     // Time delay = (128xT7 + 64xT6 + 32xT5 + 16xT4 + 8xT3 + 4xT2 + 2xT1 + T0) x 100ms
-    static const uint8_t thresh_timer = (1 << 0);
-        
+    static const uint8_t thresh_timer = 30;
+    
     PIN_OUT_HIGH(EYE0);
-    while(!max_44009_write_to_register(MAX44009_ADDRESS, MAX44009_REG_INT_EN, 0x01)) {
+    while(!max_44009_write_to_register(MAX44009_ADDRESS, MAX44009_REG_CONFIG, 0x40)) {
     }
     PIN_OUT_LOW(EYE0);
     _delay_ms(500);
+
     PIN_OUT_HIGH(EYE1);
-    while(!max_44009_write_to_register(MAX44009_ADDRESS, MAX44009_REG_THRESH_LOW, lower_threshold_lux)) {
+    while(!max_44009_write_to_register(MAX44009_ADDRESS, MAX44009_REG_THRESH_TIMER, thresh_timer)) {
     }
     PIN_OUT_LOW(EYE1);
     _delay_ms(500);
+    
     PIN_OUT_HIGH(EYE0);
-    while(!max_44009_write_to_register(MAX44009_ADDRESS, MAX44009_REG_THRESH_TIMER, thresh_timer)) {
+    while(!max_44009_write_to_register(MAX44009_ADDRESS, MAX44009_REG_THRESH_LOW, lower_threshold_lux)) {
     }
     PIN_OUT_LOW(EYE0);
+    _delay_ms(500);
+    
+    PIN_OUT_HIGH(EYE1);
+    while(!max_44009_write_to_register(MAX44009_ADDRESS, MAX44009_REG_INT_EN, 0x01)) {
+    }
+    PIN_OUT_LOW(EYE1);
 }
 
 static void reset_glowy_eyes() {
@@ -68,11 +77,16 @@ static void reset_glowy_eyes() {
     PIN_OUT_LOW(EYE1);
 }
 
-static bool is_daylight() {
+static void reset_interrupt() {
     uint8_t interrupt_status = 0;
-    while(!max_44009_read_from_register(MAX44009_ADDRESS, MAX44009_REG_INT_STAT, &interrupt_status)) {
+    max_44009_read_from_register(MAX44009_ADDRESS, MAX44009_REG_INT_STAT, &interrupt_status);
+}
+
+static bool is_daylight() {
+    uint8_t lux_low = 0;
+    while(!max_44009_read_from_register(MAX44009_ADDRESS, MAX44009_REG_LUX_HIGH, &lux_low)) {
     }
-    return !interrupt_status;
+    return (lux_low > lower_threshold_lux);
 }
 
 static void do_sleep() {
@@ -147,6 +161,7 @@ int main() {
                 event_count += 1;
             }
             while (is_daylight()) {
+                reset_interrupt();
                 // too bright. go to sleep.
                 GIMSK |= (1 << INT0);
                 sei();
